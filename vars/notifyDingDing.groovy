70db21ac629e93d996c0530eb5457cc057b1f88a
Meng JiaFeng <jiafeng.meng@merico.dev>
2022-09-28 22:44:20 +0800

    feat: init repo

---
+++ vars/notifyDingDing.groovy
@@ -0,0 +1,48 @@
+#!/usr/bin/env groovy
+
+/* notifyDingDing.groovy
+This package is used for dingding notification in jenkins pipeline
+*/
+
+def getChangeString() {
+    def changeString = ""
+    def MAX_MSG_LEN = 10
+    def changeLogSets = currentBuild.changeSets
+    for (int i = 0; i < changeLogSets.size(); i++) {
+        def entries = changeLogSets[i].items
+        for (int j = 0; j < entries.length; j++) {
+            def entry = entries[j]
+            truncatedMsg = entry.msg.take(MAX_MSG_LEN)
+            commitTime = new Date(entry.timestamp).format("yyyy-MM-dd HH:mm:ss")
+            changeString += " - ${truncatedMsg} [${entry.author} ${commitTime}]\n"
+        }
+    }
+    if (!changeString) {
+        changeString = " - No new changes"
+    }
+    return (changeString)
+}
+
+def call(String robotID, String notifyUser, String jenkinsURL, String branch, String jobName, String folder, String statusMessage, String headMessage){
+    String changeString = getChangeString()
+    wrap([$class: 'BuildUser']){
+         dingtalk (
+          robot: '${robotID}',
+          type: 'MARKDOWN',
+          title: "${jobName}[${branch}]构建通知",
+          text: [
+              "# $headMessage",
+              "# 构建详情",
+              "- 构建变更: ${changeString}"
+              "- 构建结果:${statusMessage}",
+              "- 构建人: **${env.BUILD_USER}**",
+              "- 持续时间: ${currentBuild.durationString}",
+              "- 构建日志: [日志](${env.BUILD_URL}console)",
+              "# Jenkins链接",
+              "[应用Jenkins地址](${jenkinsURL}/${folder}/job/${jobName}/)"
+
+          ],
+          at: ["${notifyUser}"]
+        )
+    }
+}
