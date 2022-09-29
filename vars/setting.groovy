24c3dab57b9d27b228f72ac7389d94e2bd6ac56e
Meng JiaFeng <jiafeng.meng@merico.dev>
2022-09-29 12:06:39 +0800

    feat: add variables

--- vars/setting.groovy
+++ vars/setting.groovy
@@ -1,6 +1,6 @@
-def configGeneral(Map args=[:]){
-    defaultSettings()
-}
+// def configGeneral(Map args=[:]){
+    // defaultSettings()
+// }
 
 def configNotify(String robotID, String atUser) {
     Config.notifySettings = [
@@ -10,13 +10,13 @@ def configNotify(String robotID, String atUser) {
 }
 
 // config default settings
-def defaultSettings() {
-    Config.settings = [
-        image_maven_version: "maven:3.8.6-openjdk-18",
-        image_buildkit_version: "moby/buildkit:master",
-        k8s_docker_config_mount_path: "/root/.docker",
-        k8s_docker_config_mount_name: "docker-config",
-    ]
-}
+// def defaultSettings() {
+    // Config.settings = [
+        // image_maven_version: "maven:3.8.6-openjdk-18",
+        // image_buildkit_version: "moby/buildkit:master",
+        // k8s_docker_config_mount_path: "/root/.docker",
+        // k8s_docker_config_mount_name: "docker-config",
+    // ]
+// }
 
-return this
+// return this
