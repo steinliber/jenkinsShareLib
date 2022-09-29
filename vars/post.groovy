aa7e23746c59b3fc8c31ab7af636b7c3495cd071
Meng JiaFeng <jiafeng.meng@merico.dev>
2022-09-29 14:38:00 +0800

    feat: add variables

---
+++ vars/post.groovy
@@ -0,0 +1,25 @@
+#!/usr/bin/env groovy
+
+def success() {
+    if(notifySettings) {
+        String successHeadMsg = "✅✅✅✅✅✅✅✅✅"
+        String successStatusMsg = "构建成功✅""
+        notify.send(successHeadMsg, successStatusMsg)
+    }
+}
+
+def failure() {
+    if(notifySettings) {
+        String failureHeadMsg = "❌❌❌❌❌❌❌❌❌"
+        String failureStatusMsg = "构建失败❌"
+        notify.send(failureHeadMsg, failureStatusMsg)
+    }
+}
+
+def aborted() {
+    if(notifySettings) {
+        String failureHeadMsg = "🟠🟠🟠🟠🟠🟠🟠"
+        String failureStatusMsg = "构建中断🟠"
+        notify.send(failureHeadMsg, failureStatusMsg)
+    }
+}
