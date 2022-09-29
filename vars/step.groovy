e6752f27dcd9c5051d3bfa51f1c70d03506f2533
Meng JiaFeng <jiafeng.meng@merico.dev>
2022-09-29 15:50:06 +0800

    feat: add variables

--- vars/step.groovy
+++ vars/step.groovy
@@ -9,53 +9,52 @@ def getProject() {
 
 def buildDockerImage(String imageAddress="moby/buildkit:master") {
     String imageRepository = Config.imageRepoSettings.get("image_repository")
-    String defaultTag = Config.imageRepoSettings.get("defaultTag")
-    String versionMethod = Config.imageRepoSettings.get("versionMethod")
-    String dockerImageSecretName = Config.imageRepoSettings.get("auth_secret_name")
-    String version = "default_version"
-    echo "----------> ${dockerImageSecretName}"
-    echo "----------> ${versionMethod}"
-    echo "----------> ${env.GIT_COMMIT}"
-    switch (versionMethod) {
-        case "commitID":
-            if (env.GIT_COMMIT) {
-                version = env.GIT_COMMIT.substring(0, 8)
-            }
-    }
-    stage("Build Docker image") {
-        if (dockerImageSecretName) {
-            podTemplate(containers: [
-                    containerTemplate(name: 'buildkit', image: "${imageAddress}", ttyEnabled: true, privileged: true),
-            ], volumes: [
-                secretVolume(secretName: "${dockerImageSecretName}", mountPath: '/root/.docker')
-            ]) {
-                node(POD_LABEL) {
-                    container('buildkit') {
-                        stage('build a Maven project') {
-                            sh """
-                              buildctl build --frontend dockerfile.v0 --local context=. --local dockerfile=. --output type=image,name=${imageRepository}:${defaultTag},push=true,registry.insecure=true
-                              buildctl build --frontend dockerfile.v0 --local context=. --local dockerfile=. --output type=image,name=${imageRepository}:${version},push=true,registry.insecure=true
-                            """
+    if (imageRepository) {
+        String defaultTag = Config.imageRepoSettings.get("defaultTag")
+        String versionMethod = Config.imageRepoSettings.get("versionMethod")
+        String dockerImageSecretName = Config.imageRepoSettings.get("auth_secret_name")
+        String version = "default_version"
+        switch (versionMethod) {
+            case "commitID":
+                if (env.GIT_COMMIT) {
+                    version = env.GIT_COMMIT.substring(0, 8)
+                }
+        }
+        stage("Build Docker image") {
+            if (dockerImageSecretName) {
+                podTemplate(containers: [
+                        containerTemplate(name: 'buildkit', image: "${imageAddress}", ttyEnabled: true, privileged: true),
+                ], volumes: [
+                    secretVolume(secretName: "${dockerImageSecretName}", mountPath: '/root/.docker')
+                ]) {
+                    node(POD_LABEL) {
+                        container('buildkit') {
+                            stage('build a Maven project') {
+                                sh """
+                                  buildctl build --frontend dockerfile.v0 --local context=. --local dockerfile=. --output type=image,name=${imageRepository}:${defaultTag},push=true,registry.insecure=true
+                                  buildctl build --frontend dockerfile.v0 --local context=. --local dockerfile=. --output type=image,name=${imageRepository}:${version},push=true,registry.insecure=true
+                                """
+                            }
                         }
                     }
                 }
-            }
-        } else {
-            podTemplate(containers: [
-                    containerTemplate(name: 'buildkit', image: "${imageAddress}", ttyEnabled: true, privileged: true),
-            ]) {
-                node(POD_LABEL) {
-                    container('buildkit') {
-                        stage('build a Maven project') {
-                            sh """
-                              buildctl build --frontend dockerfile.v0 --local context=. --local dockerfile=. --output type=image,name=${imageRepository}:${defaultTag},push=true,registry.insecure=true
-                              buildctl build --frontend dockerfile.v0 --local context=. --local dockerfile=. --output type=image,name=${imageRepository}:${version},push=true,registry.insecure=true
-                            """
+            } else {
+                podTemplate(containers: [
+                        containerTemplate(name: 'buildkit', image: "${imageAddress}", ttyEnabled: true, privileged: true),
+                ]) {
+                    node(POD_LABEL) {
+                        container('buildkit') {
+                            stage('build a Maven project') {
+                                sh """
+                                  buildctl build --frontend dockerfile.v0 --local context=. --local dockerfile=. --output type=image,name=${imageRepository}:${defaultTag},push=true,registry.insecure=true
+                                  buildctl build --frontend dockerfile.v0 --local context=. --local dockerfile=. --output type=image,name=${imageRepository}:${version},push=true,registry.insecure=true
+                                """
+                            }
                         }
                     }
                 }
-            }
 
+            }
         }
     }
 }
