a1b49278f4d0472fc28a92b66e7dd268cf60b3cc
Meng JiaFeng <jiafeng.meng@merico.dev>
2022-09-30 18:00:11 +0800

    feat: add variables

--- vars/controller.groovy
+++ vars/controller.groovy
@@ -1,9 +1,26 @@
-def testInMaven(String imageAddress="maven:3.8.1-jdk-8") {
-    if (!Config.generalSettings.skip_test) {
-        pod(stage('run mvn test') {
-                checkout scm
-                sh 'mvn -B test'
-            }
-        )
+#!/usr/bin/env groovy
+
+/* notify.groovy
+This package is used for dingding notification in jenkins pipeline
+*/
+import com.devstream.notification.Dingtalk
+import com.devstream.git.Utils
+
+/*
+send notifucation
+input params =>
+statusMessage: jenkins status message
+headMessage: jenkins head message
+*/
+def notify(String headMessage, String statusMessage) {
+    def gitUtils = new Utils()
+    String changeString = gitUtils.getChangeString()
+    switch (Config.notifySettings.notifyType) {
+        case "dingding":
+            dingtalk = new Dingtalk()
+            dingtalk.send(changeString, headMessage, statusMessage)
+            break
+        default:
+            throw new Exception("jenkins notify type ${Config.notifySettings.notifyType} doesn't support")
     }
 }
