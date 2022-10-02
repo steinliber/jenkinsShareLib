b4c7f7aba96f7b67efa3ea7b95193a203b64689b
Meng JiaFeng <jiafeng.meng@merico.dev>
2022-10-02 21:49:06 +0800

    feat: add variables

--- src/com/devstream/ci/Pod.groovy
+++ src/com/devstream/ci/Pod.groovy
@@ -4,6 +4,8 @@ package com.devstream.ci
 def testTemplate(Closure body) {
     String imageAddress = Config.generalSettings.ci_test_container_repo
     String testContainerName = Config.generalSettings.ci_test_container_name
+    println("-----> ${imageAddress}")
+    println("-----> ${Config.generalSettings.ci_test_container_repo}")
     if (!Config.generalSettings.skip_test) {
 podTemplate(
     containers: [
