d0ba80371e551521fdb5bf8453dbd9125b09121d
Meng JiaFeng <jiafeng.meng@merico.dev>
2022-10-06 18:36:39 +0800

    feat: add variables

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -22,10 +22,10 @@ def entry() {
             controller.pushCodeImage()
             post.postResult("success")
         } catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException err) {
-            println(err.getStackTrace())
+            println(err.getMessage())
             post.aborted()
         } catch (Exception err) {
-            println(err.getStackTrace())
+            println(err.getMessage())
             post.failure()
         }
     }
