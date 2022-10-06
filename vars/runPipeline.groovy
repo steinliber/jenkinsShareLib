7cf546e803079c6c25d8f7ec23b302535107b9c7
Meng JiaFeng <jiafeng.meng@merico.dev>
2022-10-06 15:54:43 +0800

    feat: add variables

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -1,20 +1,20 @@
 def call(Map config=[:]) {
-  try {
-    setting.configGeneral(config)
-    util.templates {
-      node(POD_LABEL) {
-          controller.cloneCode()
-          controller.testCode()
-          controller.sonarScan()
-          controller.pushCodeImage()
+  setting.configGeneral(config)
+  util.templates {
+    node(POD_LABEL) {
+      try {
+        controller.cloneCode()
+        controller.testCode()
+        controller.sonarScan()
+        controller.pushCodeImage()
+      } catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException err) {
+          post.aborted()
+          throw err
+      } catch (Exception err) {
+          post.failure()
+          throw err
       }
+      post.success()
     }
-  } catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException err) {
-      post.aborted()
-      throw err
-  } catch (Exception err) {
-      post.failure()
-      throw err
   }
-  post.success()
 }
